a966ee37fa769e4171830b89e45f0e167abfc4c7
Cormaxs <91635093+Cormaxs@users.noreply.github.com>
2024-03-21 23:30:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const boton = document.querySelector(".boton");
+const olMenu = document.querySelector(".olMenu");
+
+
+boton.addEventListener("click", function(){
+    olMenu.classList.toggle("activo");
+    
+})
